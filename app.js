2bb4ad32ef0cd61e47d2351d8e6a9e6f73c96951
Rahul Baliyan <rahulbaliyan863@gmail.com>
2024-12-09 00:14:24 +0530

    added cart

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ app.use(paymentRoute);
 //public folder
 
 
+
 const PORT =  process.env.PORT || 8080;
 app.listen(PORT,()=>{
   console.log(`connected to PORT : ${PORT}`)
--- controllers/cart.js
+++ controllers/cart.js
@@ -3,8 +3,7 @@ const Product  =require("../models/Product")
 const Gettocart = async(req,res)=>{
   let userId = req.user._id;
   const user = await User.findById(userId).populate('cart.product');
-  const productinfo = user.cart.map((p)=>p.product.name).join(',');
-  res.render('cart/cart',{user,productinfo})
+  res.render('cart/cart',{user,success: res.locals.success , error:  res.locals.error })
 }
 
 const Addtocart = async (req, res) => {
@@ -21,11 +20,10 @@ const Addtocart = async (req, res) => {
 
   if (productInCart) {
     productInCart.added += parseInt(added);
-    await User.updateOne(owner,{$set:{amount}})
   } else {
     user.cart.push({ product,added});
   }
-
+  await User.updateOne(owner,{$set:{amount}})
   await user.save();
 
   req.flash("success", "Product added to cart");
@@ -34,19 +32,32 @@ const Addtocart = async (req, res) => {
 
 
 
-const Deletefromcart = async(req,res)=>{
-  let{id} = req.params //product id
-  let userid = req.user._id;
-  let user =  await User.findById(userid )
-  user.cart = user.cart.filter((item)=>{
-    if((item.product._id.equals(id))){
-      return false;
-    }else{
-      return true;
+const Deletefromcart = async (req, res) => {
+  let { id } = req.params; 
+  let userId = req.user._id;
+
+  try {
+    let user = await User.findById(userId);
+    for (let i = 0; i < user.cart.length; i++) {
+      let item = user.cart[i];
+      if (item.product._id.equals(id)) {
+        const product = await Product.findById(item.product._id).populate('owner');
+        let {added} = item;
+        const owner =await User.findById(product.owner)
+        let amount  = owner.amount - product.price*added;
+        await User.updateOne(owner,{$set:{amount}})
+        user.cart.splice(i, 1);
+        break;
+      }
     }
-  })
-  await user.save()
-  res.redirect('/user/cart')
-}
+
+    await user.save();
+    req.flash('success',"Product Removed")
+    res.redirect('/user/cart');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Something went wrong while deleting the product from the cart.');
+  }
+};
 
 module.exports = {Addtocart,Gettocart,Deletefromcart};
